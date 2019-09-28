3762926d842492164da9d402978514ac75cd30c0
OlegElizarov <elizarov1999@list.ru>
2019-09-28 22:56:36 +0300

    Looks like true result

--- 1_task.cpp
+++ 1_task.cpp
@@ -5,14 +5,53 @@ using namespace std;
 int main() {
     int size=0;
     cin >>size;
-    int *arr=new int[size];
+    int *arr1=new int[size];
+    int *arr2=new int[size];
+
     for (int i=0;i<size;++i)
     {
-        cin>>arr[i];
+        cin>>arr1[i];
     }
 
+    for (int i=0;i<size;++i)
+    {
+        cin>>arr2[i];
+    }
+
+    int a=arr1[0];
+    int b=arr2[0];
+    int amax=arr1[0];
+
+    for (int i=0;i<size;++i)
+    {
+        if (arr1[i]>arr1[amax])
+        {
+            amax=i;
+        }
+        if (arr1[amax]+arr2[i]>arr1[a]+arr2[b])
+        {
+            b=i;
+            a=amax;
+        }
+    }
+
+    cout<<a<<' '<<b<<endl;
+
+
+
+
+
+
+
+
+
     for (int i = 0; i <size ; ++i) {
-        cout<<arr[i];
+        cout<<arr1[i]<<' ';
+    }
+
+    cout<<endl;
+    for (int i = 0; i <size ; ++i) {
+        cout<<arr2[i]<<' ';
     }
     return 0;
 }
