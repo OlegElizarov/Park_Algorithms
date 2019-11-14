6585a39a6ecdc973b78e6de32652f3e6919c4688
Oleg <elizarov1999@list.ru>
2019-11-14 18:12:12 +0300

    first step in 2.2

---
+++ 2.2_task.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+#include <string.h>
+
+int main(int argc, const char * argv[]) {
+    size_t n=0;
+    std:cin >> n;
+
+    while (n >=0)
+    {
+
+    }
+    return 0;
+}
