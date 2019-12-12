276fb9bf0bf59ceb551daea7ba42aa87430e89df
Oleg <elizarov1999@list.ru>
2019-12-12 17:52:03 +0300

    ArcGraph Done

--- 3.1_task.cpp
+++ 3.1_task.cpp
@@ -13,6 +13,89 @@ struct IGraph {
     virtual std::vector<int> GetPrevVertices(int vertex) const = 0;
 };
 
+class ArcGraph: public IGraph
+{
+public:
+    ArcGraph(int n): vert_arr() {}
+    ~ArcGraph(){}
+
+    ArcGraph(const IGraph &graph) : vert_arr(graph.VerticesCount())
+    {
+        for (int i = 0; i < vert_arr.size(); i++)
+        {
+            std::vector <int> vert = graph.GetNextVertices(i);
+            for (size_t j=0; j<vert.size();j++)
+            {
+                std::pair<int,int> par(i,vert[j]);
+                vert_arr.push_back(par);
+            }
+        }
+    }
+
+    void AddEdge(int from, int to) override
+    {
+        assert(0 <= from);
+        assert(0 <= to);
+        std::pair<int ,int> par(from,to);
+        vert_arr.emplace_back(from,to);
+
+    }
+
+    int VerticesCount() const override
+    {
+        std::unordered_set<int> vertic;
+        size_t count=0;
+        for (int i = 0; i < vert_arr.size() ; ++i) {
+            if (vertic.find(vert_arr[i].first) == vertic.end())
+            {
+                vertic.insert(vert_arr[i].first);
+                count++;
+            }
+            if (vertic.find(vert_arr[i].second) == vertic.end())
+            {
+                vertic.insert(vert_arr[i].second);
+                count++;
+            }
+
+        }
+        return count;
+    }
+
+    std::vector<int> GetNextVertices(int vertex) const override
+    {
+        assert(0 <= vertex && vertex < vert_arr.size());
+
+        std::vector <int> next_vert;
+
+        for (int i = 0; i < vert_arr.size() ; ++i) {
+            if (vert_arr[i].first == vertex)
+            {
+                next_vert.push_back(vert_arr[i].second);
+            }
+        }
+        return next_vert;
+    }
+
+
+    std::vector<int> GetPrevVertices(int vertex) const override
+    {
+        assert(0 <= vertex && vertex < vert_arr.size());
+        std::vector<int> prev_vertices;
+
+        for (int i = 0; i < vert_arr.size() ; ++i) {
+            if (vert_arr[i].second == vertex)
+            {
+                prev_vertices.push_back(vert_arr[i].second);
+            }
+        }
+
+        return prev_vertices;
+    }
+
+private:
+    std::vector<std::pair<int ,int>> vert_arr;
+};
+
 class SetGraph: public IGraph
 {
 public:
@@ -278,7 +361,7 @@ void print(const IGraph &graph)
 }
 
 int main(int argc, const char * argv[]) {
-    SetGraph graph(11);
+    ArcGraph graph(11);
     graph.AddEdge(0, 1);
     graph.AddEdge(0, 5);
     graph.AddEdge(1, 2);
@@ -301,7 +384,7 @@ int main(int argc, const char * argv[]) {
     print(graph);
 
     std::cout << "graph2:" << std::endl;
-    MatrixGraph graph2(graph);
+    SetGraph graph2(graph);
     print(graph2);
 
     return 0;
@@ -309,3 +392,5 @@ int main(int argc, const char * argv[]) {
 
 //0 1 5 2 3 6 4 7 8 9 10
 //10 7 9 8 0 1 5 3 6 4 2
+//0 5 1 6 4 3 2 7 9 8 10
+//10 7 8 9 0 1 3 2 5 6 4
